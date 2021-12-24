bd72498ab018416322ff14829f8a48f2cee0e5ef
Popov Aleksandr <popov654@yandex.ru>
2021-12-24 07:56:56 +0300

    Initial commit

---
+++ circle-progress.js
@@ -0,0 +1,202 @@
+﻿var st = document.createElement('style')
+st.textContent = '.circle-progress {\n' +
+    '         width: 100%;\n' +
+    '         height: 100%;\n' +
+    '         position: relative;\n' +
+    '         overflow: hidden;\n' +
+    '      }\n' +
+    '      .circle-progress > div {\n' +
+    '         position: relative;\n' +
+    '         width: 100%;\n' +
+    '         height: 100%;\n' +
+    '         overflow: hidden;\n' +
+    '         box-sizing: border-box;\n' +
+    '         padding: 2px;\n' +
+    '      }\n' +
+    '      .circle-progress > div > div {\n' +
+    '         box-sizing: border-box;\n' +
+    '         width: 100%;\n' +
+    '         height: 100%;\n' +
+    '         border-width: 30px;\n' +
+    '         border-style: solid;\n' +
+    '         border-color: #fff;\n' +
+    '         border-radius: 50%;\n' +
+    '         position: absolute;\n' +
+    '         top: 0;\n' +
+    '         left: 0;\n' +
+    '         text-align: center;\n' +
+    '      }\n' +
+    '      .circle-progress .left {\n' +
+    '         clip: rect(0 100px 200px 0);\n' +
+    '         border-color: #4b86db;\n' +
+    '         opacity: 0;\n' +
+    '         transform: rotate(180deg);\n' +
+    '         transition: transform 0.6s linear;\n' +
+    '      }\n' +
+    '      .circle-progress .right {\n' +
+    '         clip: rect(0 200px 200px 100px);\n' +
+    '         border-color: #4b86db;\n' +
+    '         opacity: 0;\n' +
+    '         transform: rotate(-180deg);\n' +
+    '         transition: transform 0.6s linear;\n' +
+    '      }\n' +
+    '      .circle-progress .left-back {\n' +
+    '         clip: rect(0 100px 200px 0);\n' +
+    '      }\n' +
+    '      .circle-progress .right-back {\n' +
+    '         clip: rect(0 200px 200px 100px);\n' +
+    '      }\n' +
+    '      .circle-progress .text {\n' +
+    '         font-size: 2rem;\n' +
+    '         line-height: 2rem;\n' +
+    '         position: absolute;\n' +
+    '         top: 40%;\n' +
+    '         font-family: Arial, Helvetica, Sans-Serif;\n' +
+    '         text-align: center;\n' +
+    '      }'
+document.head.appendChild(st)
+
+window['$circle_loader'] = function(el, col, backcol) {
+
+   el.innerHTML = '<div class="circle-progress"><div><div class="left"></div><div class="right"></div><div class="left-back"></div><div class="right-back"></div></div><div class="text">0</div></div>'
+   el = el.children[0]
+   
+   var left = el.querySelector('.left')
+   var right = el.querySelector('.right')
+   
+   var left_back = el.querySelector('.left-back')
+   var right_back = el.querySelector('.right-back')
+   
+   right.style.opacity = '0'
+   
+   el.style.display = 'block'
+   var w = el.clientWidth
+   
+   if (backcol == 'transparent') {
+      el.children[0].style.width = (w / 2) + 'px'
+      el.children[0].style.left = (w / 2) + 'px'
+   } else {
+      left_back.style.zIndex = '1'
+      left.style.zIndex = ''
+      right.style.zIndex = '1'
+      right_back.style.zIndex = ''
+      left_back.style.opacity = '1'
+      right_back.style.opacity = '1'
+   }
+   
+   for (var i = 0; i < el.children[0].children.length; i++) {
+      if (backcol == 'transparent') {
+         el.children[0].children[i].style.width = w + 'px'
+         el.children[0].children[i].style.marginLeft = -(w / 2) + 'px'
+      }
+      el.children[0].children[i].style.borderWidth = 0.15 * w + 'px'
+   }
+   left.style.clip = 
+   left_back.style.clip = 'rect(0 ' + (w/2) + 'px ' + w + 'px 0)'
+   right.style.clip = 
+   right_back.style.clip = 'rect(0 ' + w + 'px ' + w + 'px ' + (w/2) + 'px)'
+   el.style.display = 'none'
+      
+   var text = el.querySelector('.text')
+   if (text) {
+      text.style.fontSize = text.style.lineHeight = w * 0.2 + 'px'
+   }
+   
+   return {
+      el: el,
+      val: 0,
+      backcol: backcol,
+      update: function(progress) {
+         progress = Math.max(0, Math.min(1, progress))
+         this.val = progress
+         
+         var text = this.el.querySelector('.text')
+         if (text) {
+            text.textContent = Math.min(Math.round(progress * 100), 100)
+         }
+         
+         var left = this.el.querySelector('.left')
+         var right = this.el.querySelector('.right')
+         
+         if (progress <= 0.5) {
+            if (this.el.children[0].style.width == '') {
+               var w = this.el.clientWidth
+               
+               if (this.backcol == 'transparent') {
+                  this.el.children[0].style.width = (w / 2) + 'px'
+                  this.el.children[0].style.left = (w / 2) + 'px'
+                  
+                  for (var i = 0; i < this.el.children[0].children.length; i++) {
+                     this.el.children[0].children[i].style.width = w + 'px'
+                     this.el.children[0].children[i].style.marginLeft = -(w / 2) + 'px'
+                  }
+               } else {
+                  left_back.style.zIndex = '1'
+                  right_back.style.zIndex = ''
+                  left_back.style.opacity = '1'
+                  right_back.style.opacity = '1'
+               }
+               
+            } else {
+               left_back.style.zIndex = '1'
+               left.style.zIndex = ''
+               right.style.zIndex = '1'
+               right_back.style.zIndex = ''
+               left_back.style.opacity = '1'
+               right_back.style.opacity = '1'
+            }
+            
+            var deg = -180 + 360 * progress
+            left.style.opacity = '0'
+            left.style.transform = 'rotate(180deg)'
+            
+            right.style.opacity = '1'
+            right.style.transform = 'rotate(' + deg + 'deg)'
+         } else {
+            if (this.el.children[0].style.width != '' && backcol == 'transparent') {
+               this.el.children[0].style.width = ''
+               this.el.children[0].style.left = ''
+               for (var i = 0; i < this.el.children[0].children.length; i++) {
+                  this.el.children[0].children[i].style.width = ''
+                  this.el.children[0].children[i].style.marginLeft = ''
+               }
+               left_back.style.zIndex = '-1'
+            } else if (backcol != 'transparent') {
+               left_back.style.zIndex = '0'
+               left.style.zIndex = '1'
+               right.style.zIndex = '1'
+               right_back.style.zIndex = ''
+               left_back.style.opacity = '1'
+               right_back.style.opacity = '1'
+            }
+            
+            right.style.opacity = '1'
+            right.style.transform = 'rotate(0deg)'
+         
+            var deg = 180 + 360 * (progress - 0.5)
+            
+            left.style.opacity = '1'
+            left.style.transform = 'rotate(' + deg + 'deg)'
+         }
+         return this
+      },
+      show: function() {
+         this.el.style.display = ''
+         this.el.style.visibility = 'visible'
+         this.el.style.opacity = '1'
+         return this
+      },
+      hide: function() {
+         this.el.style.opacity = '0'
+         var el = this.el
+         setTimeout(function() {
+            el.style.display = 'none'
+         }, 1000)
+         return this
+      }, 
+      reset: function() {
+         this.update(0)
+         return this
+      }
+   }
+}
